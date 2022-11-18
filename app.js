f23c6cf688ec4baa8b6c30e9d02599c0abd0dfe1
Syamsul Zaman <thesyamsulzaman@gmail.com>
2022-11-18 09:09:59 +0700

    fix: add transition

--- app.js
+++ app.js
@@ -3,4 +3,10 @@ const popup = document.querySelector(".popup");
 
 hamburgerButton.addEventListener("click", () => {
   popup.classList.toggle("active");
+
+  if (popup.classList.contains("active")) {
+    hamburgerButton.querySelector("img").src = "./assets/close.svg";
+  } else {
+    hamburgerButton.querySelector("img").src = "./assets/hamburger.svg";
+  }
 })
